5dae2136b0e10f8d415b638fc9db0f2315f16d09
ATG-5222 <A01275222@tec.mx>
2022-03-19 21:41:55 -0600

    Correcion de caso3 nums

--- ACT3.2/app.cpp
+++ ACT3.2/app.cpp
@@ -11,7 +11,7 @@ set<char> alpha = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', '
 
 set<char> num = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
-set<char> sign = {'+', '-', '=', '*', '/', '^', '(', ')'};
+set<char> sign = {'+', '-', '=', '*', '/', '^', '(', ')','.'};
 
 class numbers{		//para identificar números enteros y decimales
 	
